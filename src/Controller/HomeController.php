f73d235340aa6479926eaa95a957aad17b05e716
headzoo <sean@headzoo.io>
2019-04-16 02:30:47 -0400

    Adds images

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -12,7 +13,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index")
      *
-     * @return string
+     * @return Response
      */
     public function indexAction()
     {
