e15ff1200b92e4fbafc07a3fe0f511370359a131
headzoo <sean@headzoo.io>
2019-04-16 00:24:46 -0400

    Adds required bundles

---
+++ src/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
