6ae5e4abbea5897032a791b3bd4953db679ab881
headzoo <sean@headzoo.io>
2019-04-20 15:05:26 -0400

    Adds CategoryController

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Server;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(name="category_")
+ */
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/category/{slug}", name="index")
+     *
+     * @param string $slug
+     *
+     * @return Response
+     */
+    public function indexAction($slug)
+    {
+        $category = $this->em->getRepository(Category::class)->findBySlug($slug);
+        if (!$category) {
+            throw $this->createNotFoundException();
+        }
+
+        $query = $this->em->getRepository(Server::class)
+            ->createQueryBuilder('s')
+            ->leftJoin('s.categories', 'category')
+            ->where('s.isEnabled = 1')
+            ->andWhere('category = :category')
+            ->setParameter(':category', $category);
+
+        return $this->render('category/index.html.twig', [
+            'servers'  => $this->paginate($query),
+            'category' => $category
+        ]);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Server;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,33 +23,6 @@ class HomeController extends Controller
 
         return $this->render('home/index.html.twig', [
             'servers' => $this->paginate($query)
-        ]);
-    }
-
-    /**
-     * @Route("/category/{slug}", name="category")
-     *
-     * @param string $slug
-     *
-     * @return Response
-     */
-    public function categoryAction($slug)
-    {
-        $category = $this->em->getRepository(Category::class)->findBySlug($slug);
-        if (!$category) {
-            throw $this->createNotFoundException();
-        }
-
-        $query = $this->em->getRepository(Server::class)
-            ->createQueryBuilder('s')
-            ->leftJoin('s.categories', 'category')
-            ->where('s.isEnabled = 1')
-            ->andWhere('category = :category')
-            ->setParameter(':category', $category);
-
-        return $this->render('home/category.html.twig', [
-            'servers'  => $this->paginate($query),
-            'category' => $category
         ]);
     }
 }
