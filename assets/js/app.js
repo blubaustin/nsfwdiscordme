7b51d776502bec69e1bff8e324f59e19a0e034c1
headzoo <sean@headzoo.io>
2019-04-20 19:17:07 -0400

    Adds ServerBumpsCommand

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,10 @@ $(() => {
     setInterval(() => {
       const now      = new Date().getTime();
       const distance = countDownDate - now;
+      if (distance < 60) {
+        $item.text('In a moment...');
+      }
+
       const days     = Math.floor(distance / (1000 * 60 * 60 * 24));
       const hours    = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes  = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
---
+++ src/Command/ServerBumpsCommand.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Command;
+
+use App\Entity\Server;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ServerBumpsCommand
+ */
+class ServerBumpsCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'app:server:bumps';
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    /**
+     * Constructor
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|void|null
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $now = new DateTime();
+        $serverRepository = $this->em->getRepository(Server::class);
+        foreach($serverRepository->findAll() as $server) {
+            if ($server->getDateNextBump() < $now) {
+                $server->setDateNextBump(new DateTime('24 hours'));
+            }
+        }
+
+        $this->em->flush();
+
+        $output->writeln('Done!');
+    }
+}
