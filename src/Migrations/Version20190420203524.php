299843b2b330da8c1737537bdccfc048ed781034
headzoo <sean@headzoo.io>
2019-04-21 06:37:57 -0400

    Updates migration

--- src/Migrations/Version20190420203524.php
+++ src/Migrations/Version20190420203524.php
@@ -26,7 +26,7 @@ final class Version20190420203524 extends AbstractMigration
         $this->addSql('CREATE TABLE tag (id INT UNSIGNED AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, date_created DATETIME NOT NULL, date_updated DATETIME NOT NULL, UNIQUE INDEX UNIQ_389B7835E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE server_tags ADD CONSTRAINT FK_E43CE5C61844E6B7 FOREIGN KEY (server_id) REFERENCES server (id)');
         $this->addSql('ALTER TABLE server_tags ADD CONSTRAINT FK_E43CE5C6BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id)');
-        $this->addSql('DROP INDEX search ON server');
+        // $this->addSql('DROP INDEX search ON server');
     }
 
     public function down(Schema $schema) : void
@@ -37,6 +37,6 @@ final class Version20190420203524 extends AbstractMigration
         $this->addSql('ALTER TABLE server_tags DROP FOREIGN KEY FK_E43CE5C6BAD26311');
         $this->addSql('DROP TABLE server_tags');
         $this->addSql('DROP TABLE tag');
-        $this->addSql('CREATE FULLTEXT INDEX search ON server (name, summary, description)');
+        // $this->addSql('CREATE FULLTEXT INDEX search ON server (name, summary, description)');
     }
 }
