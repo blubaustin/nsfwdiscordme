7b4b2cb09929de58c06ccd709e0093044be3f781
headzoo <sean@headzoo.io>
2019-04-22 07:09:35 -0400

    Start of bot

---
+++ bot/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use CharlotteDunois\Yasmin\Client;
+use React\EventLoop\Factory;
+
+require __DIR__ . '/../config/bootstrap.php';
+
+$loop = Factory::create();
+$client = new Client([], $loop);
+
+$client->on('ready', function () use ($client) {
+    echo 'Successfully logged into '.$client->user->tag.PHP_EOL;
+});
+
+$client->login($_SERVER['DISCORD_BOT_TOKEN']);
+$loop->run();
