2b4f660676362d9ccdbef99d52960ac5c86b3bee
headzoo <sean@headzoo.io>
2019-04-28 19:13:22 -0400

    Fixes migration

--- src/Migrations/Version20190428210358.php
+++ src/Migrations/Version20190428210358.php
@@ -25,7 +25,7 @@ final class Version20190428210358 extends AbstractMigration
         $this->addSql('DROP INDEX UNIQ_8D93D649A0D96FBF ON user');
         $this->addSql('DROP INDEX UNIQ_8D93D64992FC23A8 ON user');
         $this->addSql('DROP INDEX UNIQ_8D93D649C05FB297 ON user');
-        $this->addSql('ALTER TABLE user ADD date_last_login DATETIME NOT NULL, DROP username, DROP username_canonical, DROP email, DROP email_canonical, DROP salt, DROP password, DROP last_login, DROP confirmation_token, DROP password_requested_at, DROP googleAuthenticatorSecret');
+        $this->addSql('ALTER TABLE user ADD date_last_login DATETIME NOT NULL, DROP username, DROP username_canonical, DROP email, DROP email_canonical, DROP salt, DROP password, DROP last_login, DROP confirmation_token, DROP password_requested_at');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +33,7 @@ final class Version20190428210358 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD username VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD username_canonical VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD email VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD email_canonical VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD salt VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD password VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, ADD last_login DATETIME DEFAULT NULL, ADD confirmation_token VARCHAR(180) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD password_requested_at DATETIME DEFAULT NULL, ADD googleAuthenticatorSecret VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, DROP date_last_login');
+        $this->addSql('ALTER TABLE user ADD username VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD username_canonical VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD email VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD email_canonical VARCHAR(180) NOT NULL COLLATE utf8mb4_unicode_ci, ADD salt VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD password VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, ADD last_login DATETIME DEFAULT NULL, ADD confirmation_token VARCHAR(180) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD password_requested_at DATETIME DEFAULT NULL COLLATE utf8mb4_unicode_ci, DROP date_last_login');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649A0D96FBF ON user (email_canonical)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64992FC23A8 ON user (username_canonical)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649C05FB297 ON user (confirmation_token)');
