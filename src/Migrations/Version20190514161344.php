e019c55c0b277dfc4686fd8e0f67f6fe77a8c36f
headzoo <sean@headzoo.io>
2019-05-14 12:23:10 -0400

    Change to migration

--- src/Migrations/Version20190514161344.php
+++ src/Migrations/Version20190514161344.php
@@ -22,7 +22,9 @@ final class Version20190514161344 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD date_updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD date_updated DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE user SET date_updated = NOW()');
+        $this->addSql('ALTER TABLE user CHANGE date_updated date_updated DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
